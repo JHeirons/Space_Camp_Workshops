24dab37a3b8a7dc76f4b1d15d45cab01507f6624
JHeirons <j.r.heirons@gmail.com>
2017-07-06 14:08:09 +0100

    Lables

--- plot.py
+++ plot.py
@@ -28,18 +28,19 @@ y = data['altitude']
 x = [datetime.datetime.strptime(elem, '%H:%M:%S') for elem in x]
 
 #   Plot the data you want to see
+fig, ax1 = plt.subplots()
 plt.plot(x, y)
 
 # Sets the the intervals and the way the data is shown on the x axis of the graph
-plt.xaxis.set_major_locator(mdates.MinuteLocator(interval=10))
-plt.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-plt.xaxis.set_minor_locator(mdates.MinuteLocator(interval=5))
+ax1.xaxis.set_major_locator(mdates.MinuteLocator(interval=20))
+ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+ax1.xaxis.set_minor_locator(mdates.MinuteLocator(interval=5))
 
 #   Set your titles and axis labels for the graph
-plt.set_title('Altitude vs Time')
-plt.setp(plt.get_xticklabels(), rotation=45, horizontalalignment='right')
-plt.set_ylabel('Altitude (m)')
-plt.set_xlabel('Time')
+ax1.set_title('Altitude vs Time')
+ax1.set_ylabel('Altitude (m)')
+ax1.set_xlabel('Time')
+plt.setp(ax1.get_xticklabels(), rotation=45, horizontalalignment='right')
 
 
 plt.show()
